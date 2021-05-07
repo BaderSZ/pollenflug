f7743e181c2ce5c3dbe288de5005a1cdb1e32e1d
Bader Zaidan <bzaidan@studentsforliberty.org>
2021-05-07 13:50:08 +0200

    Break ENG array to multiple lines

--- pollenflug.py
+++ pollenflug.py
@@ -20,7 +20,8 @@ config = configparser.ConfigParser()
 config.read(str(Path.home()) + "/.pollenflug.ini")
 
 REQ_URL = "https://allergie.hexal.de/pollenflug/vorhersage/load_pollendaten.php"
-ENG_LIST = ["Ambrosia","Dock","Artemisia","Birch","Beech","Oak","Alder","Ash","Grass","Hazel","Popplar","Rye","Elm","Plantain","Willow"]
+ENG_LIST = ["Ambrosia","Dock","Artemisia","Birch","Beech","Oak","Alder","Ash","Grass",
+            "Hazel","Popplar","Rye","Elm","Plantain","Willow"]
 
 # Define input options
 SHORT_OPT = "d:p:hve"
