7364664724bb8c54623f41bbefe3a8d19c0d4b70
Bader Zaidan <bader.zaidan@rwth-aachen.de>
2022-02-16 22:02:01 +0100

    Update setup.py to version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open ("README.md", "r") as f:
 
 setuptools.setup(
     name = "pollenflug",
-    version = "0.1",
+    version = "0.1.1",
     scripts = ["pollenflug"],
     author = "Bader Zaidan",
     author_email = "bader.zaidan@rwth-aachen.de",
