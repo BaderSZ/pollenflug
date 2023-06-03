317018cc4990c7d7b14faf4fef4669fe02556cbe
Bader Zaidan <bader@zaidan.tech>
2023-06-03 14:21:15 +0200

    Fix unit tests and linting
    - environment setup changed, use black instead of autopep8
    - tests now run with pytest instead of unittest

--- tests/tests.py
+++ tests/tests.py
@@ -3,70 +3,63 @@
 import sys
 
 from pathlib import Path
-import unittest
 
 DIR = str(Path(__file__).parent.resolve())
 sys.path.append(DIR + "/../")
 
-from lib.functions import loadconfig
-from lib.color import Color
+from pollenflug.lib.functions import loadconfig
+from pollenflug.lib.color import Color
 
 
-class TestStringMethods(unittest.TestCase):
-    """Unit test class containing test functions.
+def test_colors():
+    """Test the Color class. Specifically go for:
 
-    So far, only 1 class and one function are tested.
+    - format_color() function
+    - value to Enum conversion
     """
+    # Test default = no coloring
+    assert Color.format_color("Hello, World!") == "Hello, World!"
 
-    def test_colors(self):
-        """Test the Color class. Specifically go for:
+    # Test color<-> number conversion
+    assert Color("0") == Color.GREEN
+    assert Color("1") == Color.ORANGE
+    assert Color("2") == Color.ORANGE_TOO
+    assert Color("3") == Color.RED
 
-        - format_color() function
-        - value to Enum conversion
-        """
-        # Test default = no coloring
-        self.assertEqual(Color.format_color("Hello, World!"), "Hello, World!")
-
-        # Test color<-> number conversion
-        self.assertEqual(Color("0"), Color.GREEN)
-        self.assertEqual(Color("1"), Color.ORANGE)
-        self.assertEqual(Color("2"), Color.ORANGE_TOO)
-        self.assertEqual(Color("3"), Color.RED)
-
-        # Test some function colors
-        self.assertEqual(Color.format_color("Hello, nothing!"), "Hello, nothing!")
-        self.assertEqual(Color.format_color(
-            "Hello, Red!", Color.RED), "\033[91mHello, Red!\033[0m")
-        self.assertEqual(Color.format_color(
-            "Hello, Green!", Color.GREEN), "\033[92mHello, Green!\033[0m")
-        self.assertEqual(Color.format_color("Hello, Orange!",
-                                            Color.ORANGE), "\033[93mHello, Orange!\033[0m")
-
-    def test_config(self):
-        """Test config loading. Both with and without file"""
-
-        # Test without a config
-        plz, eng, debug = loadconfig("/tmp/fakeconfigfile69420")
-
-        self.assertEqual(plz, 20095)
-        self.assertEqual(eng, True)
-        self.assertEqual(debug, False)
-
-        # Test using the test config.
-        plz, eng, debug = loadconfig(DIR + "/testconfig.ini")
-
-        self.assertEqual(plz, 55555)
-        self.assertEqual(eng, False)
-        self.assertEqual(debug, True)
-
-        # Test using 2nd config.
-        # Note! default values should be used for undefined entries
-        plz, eng, debug = loadconfig(DIR + "/testconfig_missingval.ini")
-
-        self.assertEqual(plz, 44331)
-        self.assertEqual(eng, False)
-        self.assertEqual(debug, False)
+    # Test some function colors
+    assert Color.format_color("Hello, nothing!") == "Hello, nothing!"
+    assert Color.format_color("Hello, Red!", Color.RED) == "\033[91mHello, Red!\033[0m"
+    assert (
+        Color.format_color("Hello, Green!", Color.GREEN)
+        == "\033[92mHello, Green!\033[0m"
+    )
+    assert (
+        Color.format_color("Hello, Orange!", Color.ORANGE)
+        == "\033[93mHello, Orange!\033[0m"
+    )
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_config():
+    """Test config loading. Both with and without file"""
+
+    # Test without a config
+    plz, eng, debug = loadconfig("/tmp/fakeconfigfile69420")
+
+    assert plz == 20095
+    assert eng == True
+    assert debug == False
+
+    # Test using the test config.
+    plz, eng, debug = loadconfig(DIR + "/testconfig.ini")
+
+    assert plz == 55555
+    assert eng == False
+    assert debug == True
+
+    # Test using 2nd config.
+    # Note! default values should be used for undefined entries
+    plz, eng, debug = loadconfig(DIR + "/testconfig_missingval.ini")
+
+    assert plz == 44331
+    assert eng == False
+    assert debug == False
